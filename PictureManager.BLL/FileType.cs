8058441fb0e6c2ec12e3d6c6c7c5d37db3cde8f5
Kamil Rozanski <motomc1@gmail.com>
2020-04-04 11:08:24 +0100

    cearted class picture

---
+++ PictureManager.BLL/FileType.cs
@@ -0,0 +1,13 @@
+﻿namespace PictureManager.BLL
+{
+    public enum FileType
+    {
+        UNDEFINED = 0,
+        JPG = 1,
+        JPEG = 2,
+        PNG = 3,
+        GIF = 4,
+        BMP = 5,
+        SVG = 6
+    }
+}   
---
+++ PictureManager.BLL/Picture.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PictureManager.BLL
+{
+    public class Picture
+    {
+        public Picture(string sourceFilePath)
+        {
+            var fileInfo = new FileInfo(sourceFilePath);
+            Name = fileInfo.Name;
+            FileSize = fileInfo.Length;
+            FileType = GetFileTypeByExtension(fileInfo.Extension);
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FilePath { get; set; }
+        public long FileSize { get; set; }
+        public FileType FileType { get; set; }
+
+        private FileType GetFileTypeByExtension(string extension)
+        {
+            var ext = extension.ToLower();
+
+            if (ext == "jpg")
+            {
+                return FileType.JPG;
+            }
+            else if (ext == "jpeg")
+            {
+                return FileType.JPEG;
+            }
+            else if (ext == "png")
+            {
+                return FileType.PNG;
+            }
+            else if (ext == "gif")
+            {
+                return FileType.GIF;
+            }
+            else if (ext == "bmp")
+            {
+                return FileType.BMP;
+            }
+            else if (ext == "svg")
+            {
+                return FileType.SVG;
+            }
+            else
+            {
+                return FileType.UNDEFINED;
+            }
+        }
+    }
+
+    public class Test
+    {
+        public void CreatePicture()
+        {
+            var picture = new Picture("c:\\image.jpg");
+        }
+    }
+}
