ba18d610785e3b8ba38979bc91d0949d374f1a70
Mikolaj <mikolaj.bednarek@gmail.com>
2020-04-04 16:00:15 +0200

    added first migration

---
+++ PictureManager.DAL/Migrations/20200404135922_Initial setup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PictureManager.DAL.Migrations
+{
+    public partial class Initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Picture",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    FilePath = table.Column<string>(nullable: true),
+                    FileSize = table.Column<long>(nullable: false),
+                    FileType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Picture", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Picture");
+        }
+    }
+}
